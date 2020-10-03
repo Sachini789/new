2fe755c08c0db96cc59b84baa9eb78db8bdf8c0a
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-10-03 11:26:32 +0530

    Add Crur

--- app/src/main/java/com/example/foodieapp/MManage.java
+++ app/src/main/java/com/example/foodieapp/MManage.java
@@ -6,22 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
-import com.example.foodieapp.menu.AddMenu;
+import com.example.foodieapp.menu.MenuDashboard;
 
 public class MManage extends AppCompatActivity {
-    Button addMenu;
+    Button addMenu,adOurMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m_manage);
         addMenu = findViewById(R.id.addMenu);
+        adOurMenu = findViewById(R.id.adOurMenu);
         addMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MManage.this, AddMenu.class);
+                Intent i = new Intent(MManage.this, MenuDashboard.class);
+                startActivity(i);
+            }
+        });
+        adOurMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MManage.this, OurMenu.class);
                 startActivity(i);
             }
         });
